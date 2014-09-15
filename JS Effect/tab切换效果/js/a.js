4c1bf9b42ffd067785f28c935a716807a7495e6f
Julyyq <colorfuljuly@gmail.com>
2014-09-15 22:38:39 +0800

    tab切换效果

---
+++ JS Effect/tab切换效果/js/a.js
@@ -0,0 +1,11 @@
+﻿$(function() {
+  $(".tabs-title > li").click(function() {
+    //选中效果
+    $(".tabs-title > li").removeClass("cur");
+    $(this).addClass("cur");
+    //切换内容
+    $(".tabs-content > div").hide();
+    var index = $(this).index(".tabs-title > li");
+    $(".tabs-content > div").eq(index).fadeIn();
+  });
+});
