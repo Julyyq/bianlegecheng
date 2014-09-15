0fc09f49bb9447c0646be090955d45f816d02faa
Julyyq <colorfuljuly@gmail.com>
2014-09-15 23:16:11 +0800

    悬浮登录框

---
+++ JS Effect/悬浮登录框/js/a.js
@@ -0,0 +1,10 @@
+﻿$(function() {
+  $(".login").click(function() {
+    $("#popup-login-maks").show();
+    $("#popup-login").show();
+  });
+  $("#popup-login-maks").click(function() {
+    $("#popup-login-maks").hide();
+    $("#popup-login").hide();
+  });
+});
